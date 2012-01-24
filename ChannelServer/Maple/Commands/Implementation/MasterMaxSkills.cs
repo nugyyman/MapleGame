c145675d19bee3f4371600a9681f9a12100d740e
nugyyman <nugyyman@22f4eea3-df18-4490-a9d7-79e6856d3c20>
2012-01-24 17:31:32 +0000

    Forgot to add this lol.
    git-svn-id: https://xp-dev.com/svn/MapleGame@24 22f4eea3-df18-4490-a9d7-79e6856d3c20

---
+++ ChannelServer/Maple/Commands/Implementation/MasterMaxSkills.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Loki.Maple.Characters;
+
+namespace Loki.Maple.Commands.Implementation
+{
+    class MasterMaxSkills : Command
+    {
+        public override bool IsRestricted { get { return true; } }
+        public override string Name { get { return "maxskills"; } }
+        public override string Parameters { get { return string.Empty; } }
+
+        public override void Execute(Character caller, string[] args)
+        {
+            if (args.Length != 0)
+            {
+                ShowSyntax(caller);
+            }
+            else
+            {
+                caller.MaxSkills();
+            }
+        }
+    }
+}
